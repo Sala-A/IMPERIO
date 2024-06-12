29695a306f4e188e48390e83baa52ea5cfcd0359
Sala-A <villegas.febrero@gmail.com>
2024-06-12 15:46:29 -0500

    Arreglo del Footer, cambio en efectos y organización

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -1,25 +1,37 @@
 export default function Footer() {
   return (
     <footer className="footer">
-    <div className="company_info">
-    <div className="social-media">
-        <a href="#"><img src="../src/assets/Instagram.svg" alt="Instagram" /></a>
-        <a href="#"><img src="../src/assets/Facebook.svg" alt="Instagram" /></a>
+      <div className="company_info">
+        <div className="social-media">
+          <a id="a_socialMedia" href="#">
+            <img id="img-icon" title="Instagram" src="../src/assets/Instagram.svg" alt="Instagram" />
+          </a>
+          <a id="a_socialMedia" href="#">
+            <img id="img-icon" title="Facebook" src="../src/assets/Facebook.svg" alt="Facebook" />
+          </a>
+        </div>
+        <div className="company-name">
+          <a href="#" id="name"><h1 id="company-name">IMPERIO</h1></a>
+        </div>
       </div>
-      <div className="company-name">
-        <a href="#"></a><h1>IMPERIO</h1>
-      </div>
-    </div>
       <hr />
       <nav className="footer-nav">
-        <a className="a_footer" href="#">Home</a>
-        <a className="a_footer" href="#">Nosotros</a>
-        <a className="a_footer" href="#">Tienda</a>
-        <a className="a_footer" href="#">Contacto</a>
+        <a className="a_footer" href="#">
+          Home
+        </a>
+        <a className="a_footer" href="#">
+          Nosotros
+        </a>
+        <a className="a_footer" href="#">
+          Tienda
+        </a>
+        <a className="a_footer" href="#">
+          Contacto
+        </a>
       </nav>
       <div className="footer-copy">
         <p>Daniela 2024, todos los derechos reservados.</p>
       </div>
     </footer>
-  )
+  );
 }
