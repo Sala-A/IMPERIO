4c87f8ae684ba156feaa10a6c428802cc4c58ecc
Sala-A <villegas.febrero@gmail.com>
2024-08-04 15:21:54 -0500

    Modificación de main, para corregir errores de duplicado

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,19 @@
-import React from 'react'
-import './Style/Style.scss'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Home from './Components/Page/Home'
-import Error from './Components/Page/Error'
+import "./Style/Style.scss";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Components/Page/Home";
+import Error from "./Components/Page/Error";
 
 const router = createBrowserRouter([
   {
-    path:'/',
+    path: "/",
     element: <Home />,
   },
   {
-    path:'Error',
+    path: "Error",
     element: <Error />,
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-  <RouterProvider router={router} />
-  </React.StrictMode>
-);
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
