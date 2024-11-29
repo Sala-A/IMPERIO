1227f3a642ec1304c4b8630aefa2278ba1f90365
Angi-code <angi.villegas@grupoconsultic.com.mx>
2024-11-29 16:48:04 -0500

    responsive del nav y menú hamburguesa

--- src/Components/Common/Navbar.jsx
+++ src/Components/Common/Navbar.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Search from "./Search";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Cambia el estado del menú
+  };
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${isMenuOpen ? "active" : ""}`}>
       <Link to="/" className="logo">
         <img
           title="Home"
           id="logo"
           src="./imperio.png"
+          alt="Logo"
           style={{
             width: "180px",
             height: "150px",
@@ -17,7 +25,14 @@ function Navbar() {
           }}
         />
       </Link>
-      <ul className="nav-links">
+      <button className={`hamburger ${isMenuOpen ? "active" : ""}`} onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
+
+      {/* Enlaces de navegación */}
+      <ul className={`nav-links ${isMenuOpen ? "active" : ""}`}>
         <li className="li-nav">
           <Link id="a-nav" to="/Nosotros">
             Nosotros
@@ -33,10 +48,12 @@ function Navbar() {
             Contacto
           </Link>
         </li>
+
+        {/* Buscador dentro del menú */}
+        <li className="li-nav search-user">
+          <Search />
+        </li>
       </ul>
-      <div className="search-user">
-        <Search />
-      </div>
     </nav>
   );
 }
