31ab35629fb98d6958e884c75cc91d88ee9a6c4b
Angi-code <angi.villegas@grupoconsultic.com.mx>
2024-11-29 19:23:58 -0500

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/IMPERIO/', // Cambia "IMPERIO" si el repositorio tiene otro nombre
   plugins: [react()],
-  base: '/IMPERIO/',
 });
 
